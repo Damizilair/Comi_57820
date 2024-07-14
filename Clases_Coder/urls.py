32e7e1513d2bc95ad312ede207e75f24d3e303e6
Damizilair <damizilair@gmail.com>
2024-07-14 15:55:40 -0300

    Pasamos clase 18

--- Clases_Coder/urls.py
+++ Clases_Coder/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from Clases_Coder.views import saludo, alejandro, dia_de_hoy, probando_template
+from AppCoder.views import curso
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +25,5 @@ urlpatterns = [
     path('alejo/', alejandro),
     path('dia/<dia_personalizado>', dia_de_hoy),
     path('probando_template/', probando_template),
+    path('curso/<nombre>/<numero>/', curso),
 ]
